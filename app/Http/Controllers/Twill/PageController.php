989e5e357439e53092130dc2a4eed7befaa6c550
howtomakeaturn <howtomakeaturn@hotmail.com>
2024-05-22 10:48:04 +0800

    build a simple front-end controller

--- app/Http/Controllers/Twill/PageController.php
+++ app/Http/Controllers/Twill/PageController.php
@@ -2,21 +2,23 @@
 
 namespace App\Http\Controllers\Twill;
 
+use A17\Twill\Http\Controllers\Admin\ModuleController as BaseModuleController;
 use A17\Twill\Models\Contracts\TwillModelContract;
-use A17\Twill\Services\Listings\Columns\Text;
-use A17\Twill\Services\Listings\TableColumns;
 use A17\Twill\Services\Forms\Fields\Input;
 use A17\Twill\Services\Forms\Form;
-use A17\Twill\Http\Controllers\Admin\ModuleController as BaseModuleController;
+use A17\Twill\Services\Listings\Columns\Text;
+use A17\Twill\Services\Listings\TableColumns;
 
 class PageController extends BaseModuleController
 {
     protected $moduleName = 'pages';
+
     /**
      * This method can be used to enable/disable defaults. See setUpController in the docs for available options.
      */
     protected function setUpController(): void
     {
+        $this->withoutLanguageInPermalink();
     }
 
     /**
